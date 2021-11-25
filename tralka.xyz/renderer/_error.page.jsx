b2760290a6859a411035205bee553703beae944f
mtralka <mtralka@umd.edu>
2021-11-25 14:11:13 -0500

    :lipstick: Style error pages

--- tralka.xyz/renderer/_error.page.jsx
+++ tralka.xyz/renderer/_error.page.jsx
@@ -6,15 +6,57 @@ function Page({ is404 }) {
   if (is404) {
     return (
       <>
-        <h1>404 Page Not Found</h1>
-        <p>This page could not be found.</p>
+        <section
+          h="screen"
+          w="screen"
+          display="flex"
+          flex="col"
+          align="items-center"
+          justify="center"
+          space="y-3"
+          bg="gray-600"
+          text="light-600 "
+        >
+          <h1
+            font="bold"
+          >404 Page Not Found</h1>
+          <p>This page could not be found.</p>
+          <a
+            href="/"
+            text="hover:underline purple-200"
+          > 
+            Return home
+          </a>
+        </section>
+        
       </>
     );
   } else {
     return (
+
       <>
-        <h1>500 Internal Server Error</h1>
-        <p>Something went wrong.</p>
+      <section
+          h="screen"
+          w="screen"
+          display="flex"
+          flex="col"
+          align="items-center"
+          justify="center"
+          space="y-3"
+          bg="gray-600"
+          text="light-600 "
+        >
+          <h1
+            font="bold"
+          >500 Internal Server Error</h1>
+          <p>Something went wrong.</p>
+          <a
+            href="/"
+            text="hover:underline purple-200"
+          > 
+            Return home
+          </a>
+        </section>
       </>
     );
   }
